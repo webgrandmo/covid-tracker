d943a0b076c97cdb756ec5118c84634e04c85880
@andriy.trofimov <trofimov.a@hanaboso.com>
2021-08-31 21:55:18 +0300

    Add header

---
+++ src/components/header/MainHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header class="text-center bg-green-900 text-white p-4 mb-10">
+      <div class="text-3xl md:text-5xl">
+        <i class="fas fa-head-side-mask pr-3"></i>Covid-19 Tracker
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
